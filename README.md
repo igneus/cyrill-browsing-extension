9b82a2dd8b70245c096bae242275b1afab5e3cec
Jakub Pavlík <severus@post.cz>
2018-11-16 20:58:12 +0100

    initial implementation

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cyrill Browsing Extension
+
+Firefox extension enhancing http://cyril.psalterium.cz/
+with keyboard navigation.
+(Works only on the [page detail view](http://cyril.psalterium.cz/nahled.php?id=1).)
+
+* left arrow key - go to the previous page
+* right arrow key - go to the next page
+* up arrow key - go to the year's contents
+
+This extension has only been published as source on Github,
+as the expected audience is extremely small.
+Created mainly for myself as a productivity hack while
+working on a thesis which requires browsing the site a lot.
+
+## Installation
+
+Follow the official instructions
+https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Temporary_Installation_in_Firefox
+
+## License
+
+GNU/GPL 3
---
+++ cyrill.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// are we browsing a preview of a scanned Cyrill page?
+var onPagePreview = function () {
+  return window.location.href.includes('/nahled.php?');
+};
+
+var findLinkByText = function (text) {
+  return document
+    .evaluate("//a[contains(., '" + text + "')]", document, null, XPathResult.ANY_TYPE, null )
+    .iterateNext();
+};
+
+// finds year the current page belongs to
+var extractYear = function () {
+  return document
+    .querySelector('table tr:nth-child(2) td p')
+    .innerHTML
+    .match(/Ročník: (\d{4});/)[1];
+};
+
+var previous = function () {
+  document.location.href =
+    findLinkByText('PŘEDCHOZÍ').href;
+};
+
+var next = function () {
+  document.location.href =
+    findLinkByText('NÁSLEDUJÍCÍ').href;
+};
+
+var up = function () {
+  document.location.href =
+    "http://cyril.psalterium.cz/?a=3&ListRocnik=" + extractYear();
+};
+
+document.addEventListener('keydown', (event) => {
+  const keyName = event.key;
+
+  if (!onPagePreview()) {
+    return;
+  }
+
+  if (keyName === 'ArrowLeft') {
+    previous();
+  } else if (keyName === 'ArrowRight') {
+    next();
+  } else if (keyName === 'ArrowUp') {
+    up();
+  }
+}, false);
